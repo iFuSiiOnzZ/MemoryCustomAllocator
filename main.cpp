2742408074cbc7bc9bb116eece007e584171529a
Andrei Jianu <andreisystem@gmail.com>
2015-04-18 02:46:36 +0200

    Some tests for StackAllocator

--- main.cpp
+++ main.cpp
@@ -11,63 +11,39 @@
 #define MB  1024 * 1024
 #define GB  1024 * 1024 * 1024
 
+#include <iostream>
+
 struct test_t
 {
     float x, y, z;
 };
 
+
 int main(int argc, char *argv[])
 {
-    void *l_pMem = (void *) malloc (GB);
-    CAllocator *l_pAllocator = new CStackAllocator(GB, l_pMem);
+    void *l_pMem = (void *) malloc (512);
+    CAllocator *l_pAllocator = new CStackAllocator(512, l_pMem);
 
     int *i = (int *) l_pAllocator->Allocate(sizeof(int), __alignof(int));
-    *i = 123;
-    printf("i = %d\n", *i);
-    printf("Addr i: %u\n", i);
-    printf("Used Memory: %u\n", l_pAllocator->GetUsedMemory());
-    printf("===============================================\n");
-
+    //*i = 123;
+   
     float *f = (float *) l_pAllocator->Allocate(sizeof(float), __alignof(float));
-    *f = 3.1415f;
-    printf("f = %f\n", *f);
-    printf("Addr f: %u\n", f);
-    printf("Used Memory: %u\n", l_pAllocator->GetUsedMemory());
-    printf("===============================================\n");
+    //*f = 3.1415f;
 
     test_t *t = (test_t *) l_pAllocator->Allocate(sizeof(test_t), __alignof(test_t));
-    t->x = t->y = t->z = 3.1415f;
-    printf("(x, y, z) = (%f, %f %f)\n", t->x, t->y, t->z);
-    printf("Addr t: %u\n", t);
-    printf("Used Memory: %u\n", l_pAllocator->GetUsedMemory());
-    printf("===============================================\n");
+    //t->x = t->y = t->z = 3.1415f;
+
+    //printf("i: %x\n", i);
+    //printf("f: %x\n", f);
+    //printf("t: %x\n", t);
+
+    printf("Allocated Mem: %d\n", l_pAllocator->GetUsedMemory());
 
     l_pAllocator->Deallocate(t);
-    printf("Deallocate(t)\n");
-    printf("Used Memory: %u\n", l_pAllocator->GetUsedMemory());
-    printf("===============================================\n");
-
     l_pAllocator->Deallocate(f);
-    printf("Deallocate(f)\n");
-    printf("Used Memory: %u\n", l_pAllocator->GetUsedMemory());
-    printf("===============================================\n");
-
     l_pAllocator->Deallocate(i);
-    printf("Deallocate(i)\n");
-    printf("Used Memory: %u\n", l_pAllocator->GetUsedMemory());
-    printf("===============================================\n");
 
-    t = (test_t *) l_pAllocator->Allocate(sizeof(test_t), __alignof(test_t));
-    t->x = t->y = t->z = 3.1415f;
-    printf("(x, y, z) = (%f, %f %f)\n", t->x, t->y, t->z);
-    printf("Addr t: %u\n", t);
-    printf("Used Memory: %u\n", l_pAllocator->GetUsedMemory());
-    printf("===============================================\n");
-
-    l_pAllocator->Deallocate(t);
-    printf("Deallocate(t)\n");
-    printf("Used Memory: %u\n", l_pAllocator->GetUsedMemory());
-    printf("===============================================\n");
+    printf("Allocated Mem: %d\n", l_pAllocator->GetUsedMemory());
 
     free(l_pMem);
     delete(l_pAllocator);
